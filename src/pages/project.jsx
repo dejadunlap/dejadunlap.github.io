80f1614e9a090a51886cad985d2ad59b34c1dbe1
unknown <dejareginae@gmail.com>
2025-05-20 12:36:47 -0700

    changes

--- src/pages/project.jsx
+++ src/pages/project.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Projects from '../components/Project';
-import Footer from "../components/Footer"
+
 export default function Project () {
     return (
       <div>
