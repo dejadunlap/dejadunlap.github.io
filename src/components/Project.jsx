5f8129a241b7bdffef4dc6b12273f982383e5398
unknown <dejareginae@gmail.com>
2025-05-20 12:25:01 -0700

    changes

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -5,7 +5,7 @@ export default function Projects ({ title, authors, journal, date, abstract, pap
     <div className="project">
         <p> <b>{authors}</b> {title} <i> {journal}</i> {date}</p>
         <p><b> Abstract: </b> {abstract}</p>
-        <ul>
+        <ul style={{display:'flex', flexDirection:'row', justifyContent:'flex-start', fontSize: '20px'}}>
           {paper ? (
             <li>
               <a href={paper} target="_blank" rel="noopener noreferrer"> Paper </a>
--- src/pages/project.jsx
+++ src/pages/project.jsx
@@ -6,13 +6,6 @@ export default function Project () {
       <div>
         <h1>Publications</h1>
           <ul>
-            <li><Projects title="An Association of Twitter Derived Measures of Discrimination and Race-Motivated Hate Crimes" 
-            authors="Dunlap D. and Park S."
-            journal="Journal of High School Research"
-            date="(2022)"
-            abstract="In 2020, there was a sharp rise in Anti-Asian American hate crimes. While the causes are still being explored, one of the main reasons this occurred was due to the rise in Anti-Asian American rhetoric connected to the COVID-19 pandemic. This exemplifies how prejudices often can turn into violence, more specifically hate crimes. And hateful speech, oftentimes spread through the internet, can help spread and deepen that prejudice to a wider audience. In previous research, prejuidice and discrimination has largely been studied through self-reported measures. While those studies are necessary, they are susceptible to many pitfalls and should be supplemented with statistics-based measures. This study employs one of these emerging measures. This study used the Twitter Streaming API to collect tweets containing a racial slur, an extension of hateful speech, geo-located from users in the United States over a period of two months. With these, this study compared statewide Twitter racial slur usage and statewide average sentiment polarity of the tweets with race-motivated hate crimes from the state. Though no relationship was found between statewide sentiment expressed and race-motivated hate crimes, this study did find a significant positive relationship between statewide Twitter slur usage and hate crimes. These findings support previous studies that have connected Internet-based measures of discrimination with aspects of general well-being. and even specifically hate crimes. This study shows the potential of using data derived from social media to create cost effective and expansive indicators of an area’s social climate around a topic, even race." 
-            paper="https://www.jsr.org/hs/index.php/path/article/view/2819" 
-            code="https://github.com/dejadunlap/APResearchProject"/></li>
             <li><Projects title="Evaluating Usage of African-American Vernacular English in Large Language Models" 
             authors="Dunlap D. and Mccoy R.T." 
             journal="8th AAAI/ACM Conference on AI, Ethics, and Society"
@@ -22,6 +15,13 @@ export default function Project () {
                       upbringing and culture. These results highlight the need for more diversity in the data used to train models to mitigate the potential of spreading offensive and stereotyped views of African-American language and culture." 
             paper="link" 
             code="link"/></li>
+            <li><Projects title="An Association of Twitter Derived Measures of Discrimination and Race-Motivated Hate Crimes" 
+            authors="Dunlap D. and Park S."
+            journal="Journal of High School Research"
+            date="(2022)"
+            abstract="In 2020, there was a sharp rise in Anti-Asian American hate crimes. While the causes are still being explored, one of the main reasons this occurred was due to the rise in Anti-Asian American rhetoric connected to the COVID-19 pandemic. This exemplifies how prejudices often can turn into violence, more specifically hate crimes. And hateful speech, oftentimes spread through the internet, can help spread and deepen that prejudice to a wider audience. In previous research, prejuidice and discrimination has largely been studied through self-reported measures. While those studies are necessary, they are susceptible to many pitfalls and should be supplemented with statistics-based measures. This study employs one of these emerging measures. This study used the Twitter Streaming API to collect tweets containing a racial slur, an extension of hateful speech, geo-located from users in the United States over a period of two months. With these, this study compared statewide Twitter racial slur usage and statewide average sentiment polarity of the tweets with race-motivated hate crimes from the state. Though no relationship was found between statewide sentiment expressed and race-motivated hate crimes, this study did find a significant positive relationship between statewide Twitter slur usage and hate crimes. These findings support previous studies that have connected Internet-based measures of discrimination with aspects of general well-being. and even specifically hate crimes. This study shows the potential of using data derived from social media to create cost effective and expansive indicators of an area’s social climate around a topic, even race." 
+            paper="https://www.jsr.org/hs/index.php/path/article/view/2819" 
+            code="https://github.com/dejadunlap/APResearchProject"/></li> 
           </ul>
         <h1>Datasets</h1>
         <ul>
