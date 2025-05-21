6d02d0ba6c618184d733adb3eaad0fa21982bce9
unknown <dejareginae@gmail.com>
2025-05-21 11:47:31 -0700

    test

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,6 @@ import '../styles/Footer.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
-import { Link } from "@react-email/link";
 
 
 function Footer() {
